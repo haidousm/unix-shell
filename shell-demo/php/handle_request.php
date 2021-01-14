aec13b1907d417140d1ad19e11fefc60e2a8bfac
haidousm <haidous.m@gmail.com>
2021-01-14 13:30:50 +0200

    add demo-folder for secure fiddling around with terminal

--- shell-demo/php/handle_request.php
+++ shell-demo/php/handle_request.php
@@ -5,13 +5,14 @@ if (isset($_POST["cmd"])) {
 
     $cmd = $_POST["cmd"];
 
-    if (strpos($cmd, 'rm') !== false) {
+    if (strpos($cmd, 'rm') !== false || strpos($cmd, 'cd') !== false || strpos($cmd, 'touch') !== false) {
 
-        $res = '"you", "naughty", "naughty"';
+        $res = '"you naughty naughty"';
         echo $res;
         return;
     }
     $output = NULL;
+    chdir('demo-folder');
     exec('../c/demo-shell "' . $cmd . '"', $output);
     $res = '"';
     foreach ($output as $key => $line) {
